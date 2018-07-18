7832088340ce70907f3a27b853b347c0a8217ee6
ameerthehacker <ameerjhanprof@gmail.com>
2018-07-18 16:25:29 +0530

    Add the calculator library and app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const calculator = require('./calculator');
+
+console.log("1 + 2 is " + calculator.add(1,2));
+console.log("3 - 5 is " + calculator.sub(3, 5));
+console.log("3 * 5 is " + calculator.mul(3, 5));
+console.log("5 / 2 is " + calculator.div(5, 2));
---
+++ calculator.js
@@ -0,0 +1,14 @@
+module.exports = {
+  add(a, b) {
+    return a + b;
+  },
+  sub(a, b) {
+    return a - b;
+  },
+  mul(a, b) {
+    return a * b;
+  },
+  div(a, b) {
+    return a / b;
+  }
+}
