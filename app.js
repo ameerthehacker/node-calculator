966439f324ab04a9f30b628efbbbdad47523483b
SerhatKayaa <serhat.kayaa3@gmail.com>
2019-10-03 17:39:15 +0300

    Implement log function

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ console.log("1 + 2 is " + calc.add(1,2));
 console.log("3 - 5 is " + calc.sub(3, 5));
 console.log("3 * 5 is " + calc.mul(3, 5));
 console.log("5 / 2 is " + calc.div(5, 2));
+console.log("log10 is " + calc.log(10));
--- calculator.js
+++ calculator.js
@@ -1,23 +1,38 @@
 class Calculator {
+
   add(a, b) {
     return a + b;
   }
+
   sub(a, b) {
     return a - b;
   }
+
   mul(a, b) {
     return a * b;
   }
+
   div(a, b) {
     if(b == 0) {
       throw Error("Can't divide by zero");
     }
-
     return a / b;
   }
+
   abs(a) {
     return Math.abs(a);
   }
+
+  log(a) {
+    if( a === 0 ) {
+      return "-Infinity";
+    }else if( a < 0 ) {
+      return NaN;
+    }else {
+      return Math.log(a);
+    }
+  }
+
 }
 
 module.exports = Calculator;
